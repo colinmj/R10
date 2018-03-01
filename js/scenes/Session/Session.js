49ba34e9899fb9a9afd1615a2d279d3835ff3300
Colin MJ <colinmj@telus.net>
2018-02-28 16:43:57 -0800

    Rendering the name of the speaker in the session component

--- js/scenes/Session/Session.js
+++ js/scenes/Session/Session.js
@@ -3,14 +3,14 @@ import propTypes from "prop-types";
 import { Text, View } from "react-native";
 import moment from "moment";
 
-const Session = ({ list }) => {
-  console.log(list);
+const Session = ({ list, name }) => {
   return (
     <View>
       <Text>{list.item.location}</Text>
       <Text>{list.item.title}</Text>
       <Text>{moment.unix(list.item.start_time).format("LT")}</Text>
       <Text>{list.item.description}</Text>
+      <Text> {name}</Text>
     </View>
   );
 };
--- js/scenes/Session/SessionContainer.js
+++ js/scenes/Session/SessionContainer.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Session from "./Session";
+import { formatDataObject } from "../../redux/helpers";
 
 class SessionContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      speaker: ""
+    };
   }
 
   static route = {
@@ -12,9 +16,27 @@ class SessionContainer extends Component {
     }
   };
 
+  componentDidMount() {
+    let speakerId = this.props.route.params.sessionData.item.speaker;
+    fetch(
+      `https://r10app-95fea.firebaseio.com/speakers.json?orderBy="speaker_id"&equalTo="${speakerId}"`
+    )
+      .then(res => res.json())
+      .then(name => {
+        let thisName = formatDataObject(name);
+        this.setState({ speaker: thisName.name });
+      })
+
+      .catch(err => console.log(err));
+  }
+
   render() {
-    // console.log(this.props.route.params.sessionData);
-    return <Session list={this.props.route.params.sessionData} />;
+    return (
+      <Session
+        list={this.props.route.params.sessionData}
+        name={this.state.speaker}
+      />
+    );
   }
 }
 
